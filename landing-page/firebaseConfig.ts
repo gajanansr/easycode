783824679a19bd72c61b76775eae6bb67aaa5062
Gajanan Rathod <gajanan@Gajanans-MacBook-Air.local>
2024-12-28 00:42:02 +0530

    updated firebase config

--- landing-page/firebaseConfig.ts
+++ landing-page/firebaseConfig.ts
@@ -1,14 +1,8 @@
 import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
+import { getAnalytics, isSupported } from "firebase/analytics";
 import { getFirestore, collection, addDoc } from "firebase/firestore";
 
-// Import the functions you need from the SDKs you need
-
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
-
 // Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
   apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
   authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
@@ -22,7 +16,13 @@ const firebaseConfig = {
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 
-const analytics = getAnalytics(app);
+// Initialize Firebase Analytics only on the client side
+let analytics;
+if (typeof window !== "undefined") {
+  if (isSupported()) {
+    analytics = getAnalytics(app);
+  }
+}
 
 const db = getFirestore(app);
 
